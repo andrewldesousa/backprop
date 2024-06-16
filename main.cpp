2031444b67a9e892842ec560de99e9e794043fc7
Andrew <desousa.andrew11@gmail.com>
2024-06-16 11:40:52 -0700

    fix closure bug

--- main.cpp
+++ main.cpp
@@ -11,9 +11,8 @@ public:
 
     // children set
     std::set<Scalar<T>*> children;
-    int in_degrees;
+    int in_degrees = 0;
     std::function<void()> _backward;
-
 
     Scalar(T value) : value(value), in_degrees(0) {
         _backward = []() {};
@@ -22,30 +21,29 @@ public:
     void backward() {
         // if in_degrees is not zero, return
         if (in_degrees != 0) { throw std::runtime_error("in_degrees is not zero"); }
-
-        grad = 1.0;
         _backward();
 
         // backward children
         for (auto child : children) {
             child->in_degrees--;
-            
-            if (child->in_degrees == 0) {
-                child->_backward();
-            }
+            if (child->in_degrees == 0) { child->backward(); }
         }
-
     }
 
     Scalar<T>& operator+(Scalar<T>& other) {
         Scalar<T>* result = new Scalar<T>(this->value + other.value);
-        this->children.insert(result);
-        other.children.insert(result);
-
+        
+        result->children.insert(this);
+        result->children.insert(&other);
         this->in_degrees++;
         other.in_degrees++;
 
-        result->_backward = [this, &other, &result]() {
+        result->_backward = [&, result]() {
+            // print result value and grad
+            std::cout << "result value: " << result->value << ", grad: " << result->grad << std::endl;
+            // delim 
+            std::cout << "----------------" << std::endl;
+
             this->grad += 1.0 * result->grad;
             other.grad += 1.0 * result->grad;
         };
@@ -55,13 +53,13 @@ public:
 
     Scalar<T>& operator-(Scalar<T>& other) {
         Scalar<T>* result = new Scalar<T>(this->value - other.value);
-        this->children.insert(result);
-        other.children.insert(result);
-
+        
+        result->children.insert(this);
+        result->children.insert(&other);
         this->in_degrees++;
         other.in_degrees++;
 
-        result->_backward = [this, &other, &result]() {
+        result->_backward = [&, result]() {
             this->grad += 1.0 * result->grad;
             other.grad -= 1.0 * result->grad;
         };
@@ -71,13 +69,14 @@ public:
 
     Scalar<T>& operator*(Scalar<T>& other) {
         Scalar<T>* result = new Scalar<T>(this->value * other.value);
-        this->children.insert(result);
-        other.children.insert(result);
+        
+        result->children.insert(this);
+        result->children.insert(&other);
 
         this->in_degrees++;
         other.in_degrees++;
 
-        result->_backward = [this, &other, &result]() {
+        result->_backward = [&, result]() {
             this->grad += other.value * result->grad;
             other.grad += this->value * result->grad;
         };
@@ -87,13 +86,13 @@ public:
 
     Scalar<T>& operator/(Scalar<T>& other) {
         Scalar<T>* result = new Scalar<T>(this->value / other.value);
-        this->children.insert(result);
-        other.children.insert(result);
 
+        result->children.insert(this);
+        result->children.insert(&other);
         this->in_degrees++;
         other.in_degrees++;
 
-        result->_backward = [this, &other, &result]() {
+        result->_backward = [&, result]() {
             this->grad += 1.0 / other.value * result->grad;
             other.grad -= this->value / (other.value * other.value) * result->grad;
         };
@@ -105,16 +104,11 @@ public:
 int main(int argc, char* argv[]) {
     Scalar<float> s1(1.0);
     Scalar<float> s2(2.0);
+    Scalar<float> s4(5.0);
 
     Scalar<float>& s3 = s1 + s2;
-    Scalar<float>& s4 = s1 - s2;
-    Scalar<float>& s5 = s1 * s2;
-
-    s5.backward(), s4.backward(), s3.backward();
-
-    std::cout << "s1.grad: " << s1.grad << std::endl;
-    std::cout << "s2.grad: " << s2.grad << std::endl;
-
-
+    Scalar<float>& s5 = s3 * s4;
+    s5.grad = 1.0;
+    s5.backward();
     return 0;
 }
