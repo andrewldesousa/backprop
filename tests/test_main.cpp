c4b963c5269fda4825f4d73b4ba9f18705a05d0c
Andrew <desousa.andrew11@gmail.com>
2024-06-19 20:00:59 -0700

    do grad

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -21,6 +21,28 @@ TEST(ScalarTest, Test1) {
     EXPECT_FLOAT_EQ(s5.grad, 15.0);
 }
 
+TEST(ScalarTest, Test2) {
+    Scalar<double> s1(1.0);
+    Scalar<double> s2(2.0);
+    Scalar<double> s3(2.0);
+    Scalar<double> s4(5.0);
+    Scalar<double> s5(10.0);
+
+    // expression containg all operations
+    Scalar<double>& s6 = (s1 + s2) * s3 - s4 * s5;
+
+    s6.grad = 1.0;
+    s6.backward();
+
+    // verify the gradient of s1, s2, s3, s4, s5
+    EXPECT_DOUBLE_EQ(s1.grad, 2.0);
+    EXPECT_DOUBLE_EQ(s2.grad, 2.0);
+    EXPECT_DOUBLE_EQ(s3.grad, 3.0);
+    EXPECT_DOUBLE_EQ(s4.grad, -10);
+    EXPECT_DOUBLE_EQ(s5.grad, -5);
+
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     int ret = RUN_ALL_TESTS();
